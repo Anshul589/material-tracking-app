3981149a19fa7dd7a3cf107da79d435782fda46f
Anshul589 <anshulavaiya9@gmail.com>
2025-04-30 15:16:11 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/madexam/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.madexam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/admin/admin_dashboard.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../providers/auth_provider.dart';
+import '../../screens/login_screen.dart';
+import 'materials_screen.dart';
+import 'processes_screen.dart';
+import 'users_screen.dart';
+import 'analytics_screen.dart';
+import 'reports_screen.dart';
+
+class AdminDashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Admin Dashboard'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              Provider.of<AuthProvider>(context, listen: false).logout();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (_) => LoginScreen()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Materials'),
+            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => MaterialsScreen())),
+          ),
+          ListTile(
+            title: Text('Processes'),
+            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => ProcessesScreen())),
+          ),
+          ListTile(
+            title: Text('Users'),
+            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => UsersScreen())),
+          ),
+          ListTile(
+            title: Text('Analytics'),
+            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => AnalyticsScreen())),
+          ),
+          ListTile(
+            title: Text('Reports'),
+            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => ReportsScreen())),
+          ),
+        ],
+      ),
+    );
+  }
+}
