808727d6f7aa32b0d9d687de1d47b4f8a3ac0b05
Anshul589 <147300045+Anshul589@users.noreply.github.com>
2025-04-30 15:59:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material Tracking & Costing app',
+      title: 'Material Tracking & Costing',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
